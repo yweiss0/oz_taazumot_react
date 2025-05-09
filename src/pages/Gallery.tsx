7a292023f5f8895cb6558e836932a1b2b5740b8d
yw <yweiss0@gmail.com>
2025-05-09 10:57:40 -0400

    added english subtitle video to the gallery

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -86,7 +86,7 @@ const Gallery = () => {
    
     
     { id: "1yEL4davStRjPQVJJwx5I7Ti4ki-xKbiK", type: "video", alt: "וידאו 4" },
-    // { id: "1KORO5w95xyxn_9jBk8jArym9yWOYpYgA", type: "video", alt: "5" },
+    { id: "1UTo-fVQuSNlajgmp8tLL7ktWSAZOqpBU", type: "video", alt: "5" },
     
   ];
 
